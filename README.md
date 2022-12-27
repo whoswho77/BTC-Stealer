cd19478d06b0afe934b1705f69af24f4cca4df99
whoswho77 <121280638+whoswho77@users.noreply.github.com>
2022-12-27 07:37:35 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# BTC-Stealer
-Program to scan BTC addresses looking for a balance.
+# BTC-Finder/Stealer
+Generate wallet seeds, check balance, collect.
+
 Run with build.exe to start the BTC Stealer. Generate random bitcoin wallets & private keys (seeds) and then check if they match any wallet that has a positive balance, and then take all the btc from it.
---
+++ generator.js
@@ -0,0 +1,52 @@
+"use strict";
+
+process.title = "Bitcoin Stealer by Michal2SAB";
+
+const CoinKey = require('coinkey');
+const fs = require('fs');
+
+const data = fs.readFileSync('./riches.txt');
+
+function generate() {
+    // generate random private key hex
+    let privateKeyHex = r(64);
+    
+    // create new bitcoin key pairs
+    let ck = new CoinKey(Buffer.from(privateKeyHex, 'hex'));
+    
+    ck.compressed = false;
+    //console.log(ck.publicAddress)
+    // ^ remove "//" from line above if you wanna see the logs, but remember it slows down the whole process a lot.
+        
+    // if generated wallet matches any from the riches.txt file, tell us we won!
+    if(data.includes(ck.publicAddress)){
+        console.log("");
+        process.stdout.write('\x07');
+        console.log("\x1b[32m%s\x1b[0m", ">> Success: " + ck.publicAddress);
+        var successString = "Wallet: " + ck.publicAddress + "\n\nSeed: " + ck.privateWif;
+            
+        // save the wallet and its private key (seed) to a Success.txt file in the same folder 
+        fs.writeFileSync('./Success.txt', successString, (err) => {
+            if (err) throw err; 
+        })
+            
+        // close program after success
+        process.exit();
+    }
+}
+
+// the function to generate random hex string
+function r(l) {
+    let randomChars = 'ABCDEF0123456789';
+    let result = '';
+    for ( var i = 0; i < l; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+
+console.log("\x1b[32m%s\x1b[0m", ">> Program Started and is working silently (edit code if you want logs)"); // don't trip, it works
+// run forever
+while(true){
+    generate();
+}
